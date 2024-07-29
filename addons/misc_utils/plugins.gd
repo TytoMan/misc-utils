b726fe6017bece3646ee3f2712c16ef1ce728ca3
TytoMan <47035388+TytoMan@users.noreply.github.com>
2024-07-29 16:58:34 +0900

    Add EasySave and change main scene

--- addons/misc_utils/plugins.gd
+++ addons/misc_utils/plugins.gd
@@ -3,6 +3,7 @@ extends EditorPlugin
 
 
 func _enter_tree() -> void:
+	add_custom_type("EasySave", "RefCounted", preload("res://addons/misc_utils/easy_save/easy_save.gd"), null)
 	add_custom_type("ScalarAttribute", "RefCounted", preload("scalar_attribute/scalar_attribute.gd"), null)
 	add_custom_type("Spectator3D", "Camera3D", preload("spectator/spectator3d.gd"), null)
 	add_custom_type("StateBase", "Node", preload("state_machine/state_base.gd"), null)
@@ -10,6 +11,7 @@ func _enter_tree() -> void:
 
 
 func _exit_tree() -> void:
+	remove_custom_type("EasySave")
 	remove_custom_type("ScalarAttribute")
 	remove_custom_type("Spectator")
 	remove_custom_type("StateBase")
